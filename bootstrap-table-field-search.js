23715628edfd017fe5c4935420e5ca901a2cd195
tupto <4136969+tupto@users.noreply.github.com>
2018-11-30 13:30:00 +0000

    Add bootstrap-table-field-search.js

---
+++ bootstrap-table-field-search.js
@@ -0,0 +1,60 @@
+(function($) {
+
+    'use strict';
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        searchFields: null
+    });
+    
+    var BootstrapTable = $.fn.bootstrapTable.Constructor;
+    
+    var search = function() {
+        if (this.options.searchFields == null) {
+            this.defaultSearch();
+            return;
+        }
+        
+        this.data = this.options.data;
+        var text = this.searchText;
+        if (!text) {
+            return;
+        }
+        
+        var foundItems = [];
+        var searchData = this.data;
+        this.data = [];
+        
+        var searchField = null;
+        var searchParams = text.split(':');
+        
+        if (searchParams.length > 1 && this.options.searchFields) {
+            text = text.substring(searchParams[0].length + 1, text.length).trim();
+            
+            var searchKeys = Object.keys(this.options.searchFields).map(function(key) {
+                return key.toLowerCase();
+            });
+            
+            if (searchKeys.indexOf(searchParams[0].toLowerCase()) != -1) {
+                searchField = this.options.searchFields[searchParams[0]];
+                console.log(searchField);
+            }
+        }
+        
+        var columns = this.getVisibleColumns();
+        if (searchField != null) {
+            for (var i = 0; i < searchData.length; i++) {
+                if (searchData[i][searchField] != null) {
+                    if (searchData[i][searchField].toLowerCase().indexOf(text.toLowerCase()) != -1) {
+                        this.data.push(searchData[i]);
+                    }
+                }
+            }
+        } else {
+            this.defaultSearch();
+        }
+    }
+    
+    BootstrapTable.prototype.defaultSearch = BootstrapTable.prototype.initSearch;
+    BootstrapTable.prototype.initSearch = search;
+    
+})(jQuery);
