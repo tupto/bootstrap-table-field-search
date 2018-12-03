2e26365486e513697bb6f05dd64251dd4ea8d46e
tupto <4136969+tupto@users.noreply.github.com>
2018-12-03 12:12:06 +0000

    Fix strictSearch not working as intended

--- bootstrap-table-field-search.js
+++ bootstrap-table-field-search.js
@@ -14,14 +14,14 @@
             return;
         }
         
-        this.data = this.options.data;
+        this.data = [];
         var text = this.searchText;
         if (!text) {
             return;
         }
         
         var foundItems = [];
-        var searchData = this.data;
+        var searchData = this.options.data;
         this.data = [];
         
         var searchField = null;
@@ -49,7 +49,7 @@
                 if (searchData[i][searchField] != null) {
                     if (this.options.strictSearch && text.toLowerCase() == searchData[i][searchField].toLowerCase()) {
                         this.data.push(searchData[i]);
-                    } else if (searchData[i][searchField].toLowerCase().indexOf(text.toLowerCase()) != -1) {
+                    } else if (!this.options.strictSearch && searchData[i][searchField].toLowerCase().indexOf(text.toLowerCase()) != -1) {
                         this.data.push(searchData[i]);
                     }
                 }
